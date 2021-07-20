ccf1eb4c2b3a8664df39d9c46a2cf1e4c003415c
Lucas Corte <corte1994.lc@gmail.com>
2021-07-20 13:40:07 -0300

    implementação do ponteiro de segundos

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ const updateClock = () => {
   let time = `${fixZero(now.getHours())}:${fixZero(now.getMinutes())}:${fixZero(now.getSeconds())}`
 
   digitalElement.innerHTML = time;
+
+  updateSecond(now);
 }
 
 const fixZero = (time) => {
   return time < 10 ? `0${time}` : time;
+}
+
+const updateSecond = (time) => {
+  let secDeg = ((360 / 60) * time.getSeconds()) - 90;
+  
+  secondElement.style.transform = `rotate(${secDeg}deg)`;
 }
 
 setInterval(updateClock, 1000);
